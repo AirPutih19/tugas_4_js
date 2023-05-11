eabf14f479909464e40d91e5bd826699ad5b14e7
AirPutih19 <kharismadwisj@gmail.com>
2023-05-11 16:10:29 +0700

    Tugas 4 JS
    Tugas 4 JS

---
+++ tugas4.js
@@ -0,0 +1,56 @@
+
+var a = 160;
+var b = 185;
+var c = 150;
+
+if(a>b){
+    if(a>c){
+        console.log("a adalah siswa yang tertinggi")
+        
+    }else{
+        console.log("a adalah siswa tertinggi kedua")
+
+    }
+}else{
+    if(a<c){
+        console.log("a adalah siswa terpendek")
+        
+    }else{
+        console.log("a adalah siswa tertinggi kedua")
+    }
+
+}
+if(b>c){
+    if(b>a){
+        console.log("b adalah siswa yang tertinggi")
+        
+    }else{
+        console.log("b adalah siswa tertinggi kedua")
+
+    }
+}else{
+    if(b<a){
+        console.log("b adalah siswa terpendek")
+        
+    }else{
+        console.log("b adalah siswa tertinggi kedua")
+    }
+
+}
+if(c>b){
+    if(c>a){
+        console.log("c adalah siswa yang tertinggi")
+        
+    }else{
+        console.log("c adalah siswa tertinggi kedua")
+
+    }
+}else{
+    if(c<a){
+        console.log("c adalah siswa terpendek")
+        
+    }else{
+        console.log("c adalah siswa tertinggi kedua")
+    }
+
+}
